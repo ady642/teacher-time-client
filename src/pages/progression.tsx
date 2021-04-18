758fb59f90056cbe1925b6f583b3f22795ebc1cc
Adri Haik Martos <ady642@gmail.com>
2021-04-18 22:31:03 +0200

    init providers

--- src/pages/progression.tsx
+++ src/pages/progression.tsx
@@ -1,14 +1,25 @@
 import {FunctionComponent} from "react";
 import PageWithLayoutType from "@/common/types/pageWithLayout";
 import DefaultLayout from "@/common/layouts/DefaultLayout";
-
+import { useSession, signIn, signOut } from 'next-auth/client'
 
 const Progression: FunctionComponent = () => {
-    return <div className={'flex justify-center bg-gray-100 h-full'}>
-        <div className="flex flex-col mt-10">
-            testprog
+    const [ session ] = useSession()
+
+    if(session) {
+        return <>
+            <img alt='avatar' src={session.user.image} />
+            Signed in as {session.user.name} <br/>
+            <button onClick={() => signOut()}>Sign out</button>
+        </>
+    }
+    return <>
+        Not signed in <br/>
+        <div className={'flex flex-col'}>
+            <button onClick={() => signIn('google')}>Sign In With Google</button>
+            <button onClick={() => signIn('facebook')}>Sign In With Facebook</button>
         </div>
-    </div>
+    </>
 }
 
 (Progression as PageWithLayoutType).layout = DefaultLayout
