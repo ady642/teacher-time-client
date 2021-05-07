39992437a4c35f6934ece9c425d9f20fd76fd45a
Adri Haik Martos <ady642@gmail.com>
2021-05-07 10:49:41 +0200

    Revert "add copy for webRTC3"
    This reverts commit d761fa4f8d4469f1376c74689516d74dadea0725.

--- src/modules/Room/components/atoms/AtomsFullScreenButton.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Button } from './AtomsButton';
-
-export const AtomsShareScreen = ({ isSharing =false, onToggle =() => {}}) => {
-	return <Button onClick={() => onToggle()}>{isSharing ? 'Cancel' : 'Share Screen'}</Button>;
-};
--- src/modules/Room/components/atoms/AtomsShareButton.tsx
+++
@@ -1,6 +0,0 @@
-import React, {FC} from 'react';
-import { Button } from './AtomsButton';
-
-export const AtomsFullScreenButton: FC = () => {
-	return <Button>Full Screen</Button>;
-};
