deac076f896b8cdf9ad42d45d81f2002f10f6e57
Adri Haik Martos <ady642@gmail.com>
2021-06-14 17:06:55 +0200

    little fix

--- src/modules/Room/Whiteboard/components/Toolbox/InputText.tsx
+++ src/modules/Room/Whiteboard/components/Toolbox/InputText.tsx
@@ -1,31 +1,17 @@
 import React, {
 	MutableRefObject,
 	FC,
-	
+
 } from "react";
 import TextareaAutosize from 'react-textarea-autosize';
 interface InputTextProps {
-	
+
 	textBoxRef: MutableRefObject<HTMLTextAreaElement>
-	
+
 }
 
 const InputText:FC<InputTextProps> = (props) => {
-	
-	
-
-
-	return (
-		
-		
-		
-
-				<TextareaAutosize autoFocus id="InputText" ref={props.textBoxRef } className="  w-40 box-content font-sans:Arial absolute select-all resize flex-grow border border-solid bg-transparent rounded-md tracking-normal leading-normal text-left text-opacity-25 text-lg  align-middle flex-auto  cursor-auto" ></TextareaAutosize>
-				
-		
-			
-		
-	);
+	return <TextareaAutosize autoFocus id="InputText" ref={props.textBoxRef} className="  w-40 box-content font-sans:Arial absolute select-all resize flex-grow border border-solid bg-transparent rounded-md tracking-normal leading-normal text-left text-opacity-25 text-lg  align-middle flex-auto  cursor-auto" />
 };
 
 export default InputText;
