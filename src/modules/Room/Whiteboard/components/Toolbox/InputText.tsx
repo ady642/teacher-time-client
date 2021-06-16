6f4e8ae59c673ef530cf455426e5c127a2058e20
Adri Haik Martos <ady642@gmail.com>
2021-06-16 10:52:44 +0200

    use normal textarea

--- src/modules/Room/Whiteboard/components/Toolbox/InputText.tsx
+++ src/modules/Room/Whiteboard/components/Toolbox/InputText.tsx
@@ -1,17 +1,10 @@
-import React, {
-	MutableRefObject,
-	FC,
-
-} from "react";
-import TextareaAutosize from 'react-textarea-autosize';
+import {MutableRefObject, FC} from "react";
 interface InputTextProps {
-
 	textBoxRef: MutableRefObject<HTMLTextAreaElement>
-
 }
 
 const InputText:FC<InputTextProps> = (props) => {
-	return <TextareaAutosize autoFocus id="InputText" ref={props.textBoxRef} className="  w-40 box-content font-sans:Arial absolute select-all resize flex-grow border border-solid bg-transparent rounded-md tracking-normal leading-normal text-left text-opacity-25 text-lg  align-middle flex-auto  cursor-auto" />
+	return <textarea autoFocus id="InputText" ref={props.textBoxRef} className="w-40 box-content font-sans:Arial absolute select-all resize flex-grow border border-solid bg-transparent rounded-md tracking-normal leading-normal text-left text-opacity-25 text-lg  align-middle flex-auto  cursor-auto" />
 };
 
 export default InputText;
