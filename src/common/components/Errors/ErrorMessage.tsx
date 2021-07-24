215a2634b264db36a726e6d5a1039c533237cab1
Adri Haik Martos <ady642@gmail.com>
2021-07-24 21:33:40 +0200

    add registration error if user already registered

---
+++ src/common/components/Errors/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+
+interface ErrorMessageProps {
+
+}
+
+const ErrorMessage: FunctionComponent<ErrorMessageProps> = () => {
+    return <div></div>
+}
+
+export default ErrorMessage
