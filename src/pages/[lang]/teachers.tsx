636904639f9142b6598ca71610e1fa3d6ea77a9f
Adri Haik Martos <ady642@gmail.com>
2021-08-19 09:15:04 +0200

    change hours of teaching

--- src/pages/[lang]/teachers.tsx
+++ src/pages/[lang]/teachers.tsx
@@ -86,7 +86,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 				hourlyRate: 30,
 				rating: 5,
 				hasDiploma: true,
-				description: 'Révision pour les élèves  entrant en terminale. à 15h  et à 17h tous les jours :Les dérivées : équation de la tangente : questions réponses',
+				description: 'Révision pour les élèves  entrant en terminale. à 16h  et à 18h tous les jours :Les dérivées : équation de la tangente : questions réponses',
 				avatar: '/img/drawbg.jpg',
 				name: 'Adrien HM MATHEMATIQUES',
 				languages: ['es', 'fr']
