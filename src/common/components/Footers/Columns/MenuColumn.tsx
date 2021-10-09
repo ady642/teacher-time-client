7f874fb32bf97fa148ce0921f906cac11666055d
Adri Haik Martos <ady642@gmail.com>
2021-10-09 08:08:56 +0200

    add robots.txt

--- src/common/components/Footers/Columns/MenuColumn.tsx
+++ src/common/components/Footers/Columns/MenuColumn.tsx
@@ -2,6 +2,9 @@ import {FunctionComponent} from "react";
 import FooterColumn from "@/common/components/Footers/FooterColumn";
 import useTranslation from "@/common/hooks/useTranslation";
 import FooterItem from "@/common/components/Footers/FooterItem";
+import useUserGetters from "@/context/user/helpers/useUserGetters";
+import { Switch, Case } from "react-switch-case-module"
+import AvailableSwitch from "@/modules/Teachers/List/components/AvailableSwitch";
 
 interface MenuProps {
 
@@ -9,12 +12,19 @@ interface MenuProps {
 
 const MenuColumn: FunctionComponent<MenuProps> = () => {
 	const { t } = useTranslation()
-
-	const items = [{ text: t('common.findAteacher'), url: 'teachers/list' }, { text: t('common.giveClasses'), url: 'teachers/create' }]
+	const { teacher } = useUserGetters()
 
 	return <FooterColumn title={'Menu'}>
 		<>
-			{ items.map((item) => <FooterItem  key={item.text} text={item.text} url={item.url} />) }
+			 <FooterItem  key={t('common.findAteacher')} text={t('common.findAteacher')} url={'teachers/list'} />
+			<Switch componentName={teacher ? "AvailableSwitch": 'FooterItem'} defaultComponent={<FooterItem  key={t('common.giveClasses')} text={t('common.giveClasses')} url={'teachers/create'} />}>
+				<Case value={'FooterItem'}>
+					<FooterItem  key={t('common.giveClasses')} text={t('common.giveClasses')} url={'teachers/create'} />
+				</Case>
+				<Case value={'AvailableSwitch'}>
+					<AvailableSwitch />
+				</Case>
+			</Switch>
 		</>
 	</FooterColumn>
 }
