c6aa781756d2439f2152288b01269baf2096829a
Adri Haik Martos <ady642@gmail.com>
2021-10-10 12:41:34 +0200

    change teacher card cta text

--- src/modules/Teachers/List/components/TeacherList/TeacherCard/TeacherCardThirdLine/TeacherCardCallButton.tsx
+++ src/modules/Teachers/List/components/TeacherList/TeacherCard/TeacherCardThirdLine/TeacherCardCallButton.tsx
@@ -6,7 +6,7 @@ interface TeacherCardCallButtonProps {
 
 const TeacherCardCallButton: FunctionComponent<TeacherCardCallButtonProps> = ({ onClick }) => {
 	return <button className={`rounded-3xl transition bg-orange hover:bg-red-700 text-white px-5 text-sm font-bold py-1`} onClick={() => onClick()}>
-		Poser sa question
+		Appeler
 	</button>
 }
 
